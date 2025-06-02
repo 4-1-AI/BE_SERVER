53325cd661426fa0f638ee63336ecf788b562f82
minju <minju9711@naver.com>
2025-06-03 04:59:18 +0900

    feat: 초기작업

---
+++ src/main/java/spring/firecare/FirecareApplication.java
@@ -0,0 +1,13 @@
+package spring.firecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirecareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirecareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/firecare/FirecareApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.firecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirecareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
