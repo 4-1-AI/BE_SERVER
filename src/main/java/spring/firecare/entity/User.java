67de62035d8263c9904b53471c81a4825d27e831
전선영 <minnie2002@naver.com>
2025-06-05 10:21:36 +0900

    "로그인, 회원가입, 마이페이지 수정 구현 완료"

---
+++ src/main/java/spring/firecare/entity/User.java
@@ -0,0 +1,27 @@
+package spring.firecare.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;       // 아이디
+    private String password;    // 비밀번호
+    private String nickname;    // 닉네임
+
+    @ElementCollection
+    @CollectionTable(name = "guardian", joinColumns = @JoinColumn(name = "user_id"))
+    private List<Guardian> guardians = new ArrayList<>();
+}
