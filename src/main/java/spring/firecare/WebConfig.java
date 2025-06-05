936c05f51f67aed82e6daee9ed2ec265fb0b75e5
minju <minju9711@naver.com>
2025-06-06 03:03:10 +0900

    fix: cors 경로 수정

--- src/main/java/spring/firecare/WebConfig.java
+++ src/main/java/spring/firecare/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
                         .allowedOrigins(
                                 "http://localhost:3000",
                                 "https://front-test-de68f.web.app",
-                                "http://127.0.0.1:5501"
+                                "http://127.0.0.1:5500"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
