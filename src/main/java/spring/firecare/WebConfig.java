c908e55266185e36d8482cf735e6c67e80d3bd32
minju <minju9711@naver.com>
2025-06-06 03:13:02 +0900

    fix: cors 경로 수정

--- src/main/java/spring/firecare/WebConfig.java
+++ src/main/java/spring/firecare/WebConfig.java
@@ -14,14 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로 허용
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "https://front-test-de68f.web.app",
-                                "http://127.0.0.1:5500"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
